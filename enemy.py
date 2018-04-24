6ea8a1a9ffe9b3b548ae48ff71904dbe2c0a2aa7
Kaloyan <simplyawesom@gmail.com>
2018-04-24 18:09:45 +0300

    add attack method

--- enemy.py
+++ enemy.py
@@ -3,8 +3,10 @@ from python101.dungeons_and_python.unit import Unit
 
 class Enemy(Unit):
     def __init__(self, health, mana, damage):
+        if not isinstance(damage, (int, float)):
+            raise TypeError('Damage should be a Number!')
         super().__init__(health, mana)
         self.damage = damage
 
     def attack(self):
-        pass
+        return self.damage
